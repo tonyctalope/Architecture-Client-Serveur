ae851d32f2c17e4a60b81999d2c4a17c531e5543
Lucile16 <lulubeucher@gmail.com>
2023-04-21 08:56:56 +0200

    feat(test): starting tests

--- client/src/features/views/Home.tsx
+++ client/src/features/views/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
   return (
     <Container className="mt-4">
       <div className="relative text-center px-2 mx-auto py-8">
-        <h1>Bienvenue sur Ubeer.</h1>
+        <h1>Bienvenue sur Ubeer</h1>
         <p className="mt-6">Ubeer est une plateforme de livraison de bières à domicile.</p>
         <Image
           className="bg-area z-3 m-4"
---
+++ client/src/features/views/__tests__/Home.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import { test, expect, describe } from 'vitest';
+import { Home } from '../Home';
+
+describe('Home', () => {
+  test('should render', () => {
+    const wrapper = render(<Home />);
+
+    expect(wrapper.getByText("Bienvenue sur Ubeer")).toBeDefined();
+    expect(wrapper.getByText("Ubeer est une plateforme de livraison de bières à domicile.")).toBeDefined();
+    expect(wrapper.getByRole('img')).toBeDefined();
+  });
+});
